acae1b96728e7a342a32620095a1322ab55b3d86
K1D77A <K1D77A@users.noreply.github.com>
2019-05-08 21:09:19 +0100

    functioning
    it no longer misses opcodes on their own lines

--- op-codes.lisp
+++ op-codes.lisp
@@ -1,4 +1,4 @@
-;#!/usr/bin/sbcl --script
+#!/usr/bin/sbcl --script
 ;;;;script that takes in a compiled file and grabs only the op codes from it
 
 (defparameter *path* (second sb-ext:*posix-argv*))
@@ -19,79 +19,54 @@
 	  :collect x)))
 
 (defun get-a-lines-op-codes (line)
-  "Gets the op codes from a line as characters"
-  (let ((first-tab (position #\Tab line :test #'string=))
-	(second-tab (position #\Tab line :from-end t :test #'string=)))
-    (if (and first-tab second-tab)
-	(subseq line first-tab second-tab)
-	nil)))
-
-
-(defun remove-x-from-list (x list &key (test #'string=))
-  (mapcar (lambda (entry)
-	    (remove x entry :test test))
-	  list))
-	  
+  "need to step through the string and check for a #\ #\<char> #\<char> #\. Can't use regex because that requires quicklisp"
+  (if (> (length line) 4)
+      (do* ((len (length line))
+	    (colon-found-p nil)
+	    (op-codes "")
+	    (a 0 (1+ a))
+	    (b 1 (1+ b))
+	    (c 2 (1+ c))
+	    (d 3 (1+ d))
+	    (ac (aref line a)
+		(aref line a))
+	    (bc (aref line b)
+		(aref line b))
+	    (cc (aref line c)
+		(aref line c))
+	    (dc (aref line d)
+		(aref line d)))
+	   (nil)
+	(when (some (lambda (char)
+		    (char= char #\:))
+		  (list ac bc cc dc))
+	    (setf colon-found-p t))
+	(when (and colon-found-p
+		   (and (char/= bc #\Space)
+			(char/= cc #\Space))
+		   (or (and (char= ac #\Tab)
+			    (char= dc #\Space))
+		       (and (char= dc #\Tab)
+			    (char= ac #\Space))
+		       (char= ac dc #\Space)
+		       (char= ac dc #\Tab)))
+	  (setf op-codes
+		(concatenate 'string op-codes
+			     (format nil "\\x~A~A" bc cc))))
+	(if (= d (1- len))
+	    (return op-codes)))
+      ""))
 (defun get-a-files-op-codes (file)
-  "Gets a list of op codes from file"
-  (mapcar #'get-a-lines-op-codes  file))
-(defun remove-blanks-from-op-codes-list (a-files-op-codes)
-  (loop :for x :in a-files-op-codes
-	:unless (string= x "")
-	  :collect x))
-(defun remove-nil-from-op-codes-list (op-code-list)
-  (loop :for x :in op-code-list
-	:when x
-	  :collect x))
-
-(defun remove-trailing-spaces-from-op-codes-entry (op-code-entry)
-  (subseq op-code-entry 0
-	  (1+ (position #\Space op-code-entry :from-end t :test-not #'string=))))
-(defun remove-trailing-spaces-from-op-codes-list (a-files-op-codes)
-  "Removes the trailing spaces from a-files-op-codes"
-  (mapcar #'remove-trailing-spaces-from-op-codes-entry a-files-op-codes))
-(defun hexlify-op-code (op-code)
-  "Turns b8 into \xb8 for example"
-  (concatenate 'string (format nil "\\") "x" op-code))
-(defun hexlify-op-codes-entry (op-code-entry)
-  "turns '0f05' into '\x0f\x05"
-  (coerce (remove nil
-		  (reduce #'append
-			  (do* ((op-code-list (coerce op-code-entry 'list))
-				(a (pop op-code-list)
-				   (pop op-code-list))
-				(b (pop op-code-list)
-				   (pop op-code-list))
-				(new ()))
-			       ((and (null a)(null b))
-				(reverse new))
-			  ;  (print a)
-			    (push (list #\\ #\x a b) new))))	 
-	  'string))
-
-(defun hexlify-op-codes-file (a-files-op-codes)
-  (mapcar #'hexlify-op-codes-entry a-files-op-codes))
-  
+  (mapcar #'get-a-lines-op-codes file))
 (defun process (file)
   "Does the boy work"
   (princ
    (apply #'concatenate 'string
-     (hexlify-op-codes-file
-     (remove-x-from-list #\Tab
-      (remove-x-from-list nil
-       (remove-x-from-list #\Space
-       	(remove-blanks-from-op-codes-list
-         (remove-nil-from-op-codes-list
-          (get-a-files-op-codes
-           (load-up-file file)))))
-        :test #'equal))))))
+	  (get-a-files-op-codes
+	   (load-up-file file))))
+  :test #'equal)
 
 (princ #\NewLine)
 (process *file*)
 (princ #\NewLine)
-;(defun ree (a)
- ; (let ((b (logxor #x6000c8 a)))
-  ;  (format t "~X" a)
-   ; (princ #\NewLine)
-    ;(format t "~X" b)
-   ; (logxor a b)))
+
